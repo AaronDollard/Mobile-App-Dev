76acc50a7ba3c6857d38ee51e19dd134577b4fc4
Aaron Dollard <daronaollard1995@gmail.com>
2020-11-05 18:03:06 +0000

    Added sound effect for when phone is picked up

--- app/src/main/java/edu/adollard/accelerometersensor/MainActivity.java
+++ app/src/main/java/edu/adollard/accelerometersensor/MainActivity.java
@@ -6,14 +6,21 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     TextView tvx, tvy, tvz;
     private SensorManager mSensorManager;
     private Sensor mSensor;
+    private boolean isFlat = false;
+    double flatValueX, flatValueY, flatValueZ;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +34,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // Choose the sensor you want
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
     }
 
-    /*
-     * When the app is brought to the foreground - using app on screen
-     */
+     //When the app is brought to the foreground - using app on screen
     protected void onResume() {
         super.onResume();
         // turn on the sensor
@@ -40,22 +44,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    /*
-     * App running but not on screen - in the background
-     */
+
+     //App running but not on screen - in the background
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);    // turn off listener to save power
     }
 
-    /*
-     * Called by the system every x milllisecs when sensor changes
-     */
+
+     //Called by the system every x milllisecs when sensor changes
     @Override
     public void onSensorChanged(SensorEvent event) {
         // called byt the system every x ms
         float x, y, z;
-
         x = event.values[0];    // get x value from sensor
         y = event.values[1];
         z = event.values[2];
@@ -64,10 +65,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         tvy.setText(String.valueOf(y));
         tvz.setText(String.valueOf(z));
 
+        if(x < 1 && y < 1 && z > 9){ //Toast appears when the phone is flat
+            if (isFlat == false){
+                isFlat = true;
+                flatValueX = x;
+                flatValueY = y;
+                flatValueZ = z;
+                Toast.makeText(this, "Phone is flat", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (isFlat == true) { //If the phone is flat
+            if( x != flatValueX &&  y != flatValueY && z != flatValueZ){ //If the coord aren't the same as flat coords
+                if (isFlat = true){  //If the phone is flat equals true
+                    isFlat = false; //Phone is not flat
+                    Toast.makeText(this, "Phone is not flat", Toast.LENGTH_SHORT).show();
+                    mediaPlayer = MediaPlayer.create(this, R.raw.yay);
+                    mediaPlayer.start();
+                }
+            }
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // not using
+    }
+
+    public void doReset(View view) { //Reset isFlat to false
+        isFlat = false;
     }
 }
