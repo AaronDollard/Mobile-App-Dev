e0d624cf461b14b0cb82d3b3d7f9b86235d6a77c
Aaron Dollard <daronaollard1995@gmail.com>
2020-12-17 17:51:35 +0000

    initial app setup

---
+++ app/src/main/java/edu/adollard/gameapp/HiScores.java
@@ -0,0 +1,14 @@
+package edu.adollard.gameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HiScores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hi_scores);
+    }
+}
